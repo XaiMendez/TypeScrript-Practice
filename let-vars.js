7ace6463a8f5ca9ce5bd22be10f02994e8a6ba8c
XaiMendez <bmendez@bancoazul.com>
2018-05-18 08:15:52 -0600

    adding let-vars-ts

---
+++ let-vars.js
@@ -0,0 +1,8 @@
+// var en ts, for global var
+var name = "Xavier";
+if (name == "Xavier") {
+    // let block code var, in instruction
+    var name_1 = "Pamela";
+    console.log("name: (in if) " + name_1);
+}
+console.log("name: (out if) " + name);
---
+++ let-vars.ts
@@ -0,0 +1,10 @@
+// var en ts, for global var
+var name = "Xavier";
+
+if(name == "Xavier"){
+
+	// let block code var, in instruction
+	let name = "Pamela";
+	console.log("name: (in if) " + name);
+}
+console.log("name: (out if) " + name);
